409b13ec4d93e7bfd2901ea91faba113debadec8
drilonu <86578785+drilonu@users.noreply.github.com>
2022-03-14 15:00:48 +0000

    Base file

---
+++ app.js
@@ -0,0 +1,21 @@
+let groceryItem = "apple";
+
+switch (groceryItem) {
+
+  case "tomato":
+    console.log("Tomatoes are $0.49");
+    break;
+
+  case "lime":
+    console.log("Limes are $1.49");
+    break;
+
+  case "papaya":
+    console.log("Papayas are $1.29");
+    break;
+
+  default:
+    console.log("Invalid item");
+    break;
+
+}
